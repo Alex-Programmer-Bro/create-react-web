1994d56f48e81f3a4842e0cb90f784358459c01f
Alex-Programer <alex.lichangnan@gmail.com>
2023-09-11 17:18:08 +0800

    chore: manage template

--- package/command/src/index.ts
+++ package/command/src/index.ts
@@ -1,3 +1,33 @@
 #!/usr/bin/env node
 
-console.log('hello')
+import process from 'node:process'
+import { mkdir } from 'node:fs/promises'
+import { resolve } from 'node:path'
+import { Command } from 'commander'
+import { copySync } from 'fs-extra'
+import pkg from '../package.json'
+
+const program = new Command()
+
+program
+  .name(pkg.name)
+  .description(pkg.description)
+  .version(pkg.version)
+
+program
+  .command('create')
+  .arguments('projectName')
+  .description('create a web project')
+  .action(async (projectName: string) => {
+    const targetPath = resolve(process.cwd(), projectName)
+    await mkdir(targetPath)
+    try {
+      const templatePath = resolve(__dirname, 'template')
+      copySync(templatePath, targetPath)
+    }
+    catch (err) {
+      console.error(err)
+    }
+  })
+
+program.parse()
